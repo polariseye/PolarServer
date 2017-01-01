c80ff57cac7f302e2389b772002710cc05ed180d
liujun <tom19900920@163.com>
2017-01-01 11:49:38 +0800

    再添加文件

---
+++ common/errorCode/systemErrorCode.go
@@ -0,0 +1,14 @@
+package errorCode
+
+var (
+	// 调用成功
+	Success ErrorCode = NewErrorCode(0, "调用成功")
+	// 客户端数据错误
+	ClientDataError ErrorCode = NewErrorCode(-1, "客户端数据错误")
+	// 数据错误
+	DataError ErrorCode = NewErrorCode(-2, "数据错误")
+	// 客户端调用参数不足
+	ParamNotEnough ErrorCode = NewErrorCode(-3, "客户端调用参数不足")
+	// 调用方法不存在
+	MethodNoExist ErrorCode = NewErrorCode(-4, "调用方法不存在")
+)
