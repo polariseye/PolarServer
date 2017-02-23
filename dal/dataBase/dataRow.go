74bf924a1cd25e1046a2a873c2430357107ab35d
liujun <liujun@moqikaka.com>
2017-02-23 20:59:43 +0800

    完善数据库表操作代码

--- dal/dataBase/dataRow.go
+++ dal/dataBase/dataRow.go
@@ -4,21 +4,71 @@ import (
 	"errors"
 )
 
+// 数据行结果
 type DataRow struct {
+	// 所属数据表
 	table *DataTable
 
+	// 行的所有值
 	cells []interface{}
 }
 
-func (this *DataRow) Cells() []interface{} {
+// 行的所有原始值
+func (this *DataRow) CellOriginValues() []interface{} {
 	return this.cells
 }
 
+// 值的个数
 func (this *DataRow) Len() int {
 	return len(this.cells)
 }
 
-func (this *DataRow) Cell(celIndex int) (interface{}, error) {
+// 单元格的字符串值（可能为nil）
+// celIndex:单元格序号
+// 返回值:
+// interface{}:单元格的字符串值
+// error:错误信息
+func (this *DataRow) CellValue(celIndex int) (interface{}, error) {
+	if len(this.cells) <= celIndex {
+		return nil, errors.New("cell out of range")
+	}
+
+	// 检查是否为nil
+	if this.cells[celIndex] == nil {
+		return nil, nil
+	}
+
+	// 转换为字符串
+	switch this.cells[celIndex].(type) {
+	case []byte:
+		return string(this.cells[celIndex].([]byte)), nil
+	case string:
+		return this.cells[celIndex].(string), nil
+	}
+
+	return nil, nil
+}
+
+// 单元格的字符串值（可能为nil）
+// cellName:单元格名称
+// 返回值:
+// interface{}:单元格的字符串值
+// error:错误信息
+func (this *DataRow) CellValueByName(cellName string) (interface{}, error) {
+	celIndex := this.table.cellIndex(cellName)
+	if celIndex < 0 {
+		return nil, errors.New("cell name no exist")
+	}
+
+	return this.CellValue(celIndex)
+}
+
+// 单元格的原始值
+// celIndex:单元格序号
+// 返回值:
+// interface{}:单元格的字符串值
+// error:错误信息
+func (this *DataRow) OriginCellValue(celIndex int) (interface{}, error) {
 	if len(this.cells) <= celIndex {
 		return nil, errors.New("cell out of range")
 	}
@@ -26,15 +76,23 @@ func (this *DataRow) Cell(celIndex int) (interface{}, error) {
 	return this.cells[celIndex], nil
 }
 
-func (this *DataRow) CellByName(cellName string) (interface{}, error) {
+// 单元格的原始值
+// cellName:单元格名称
+// 返回值:
+// interface{}:单元格的字符串值
+// error:错误信息
+func (this *DataRow) OriginCellValueByName(cellName string) (interface{}, error) {
 	celIndex := this.table.cellIndex(cellName)
 	if celIndex < 0 {
 		return nil, errors.New("cell name no exist")
 	}
 
-	return this.cells[celIndex], nil
+	return this.OriginCellValue(celIndex)
 }
 
+// 创建单元格对象
+// _table:所属表对象
+// _cells:单元格的值集合
 func newDataRow(_table *DataTable, _cells []interface{}) *DataRow {
 	return &DataRow{
 		table: _table,
--- dal/dataBase/dataTable.go
+++ dal/dataBase/dataTable.go
@@ -5,11 +5,19 @@ import (
 	"errors"
 )
 
+// 数据表结构
 type DataTable struct {
-	rowData     []*DataRow
+	// 行对象集合
+	rowData []*DataRow
+
+	// 列名称集合
 	columnNames map[string]int
 }
 
+// 数据表初始化
+// rows:原始的数据行信息
+// 返回值：
+// error:初始化的错误信息
 func (this *DataTable) init(rows *sql.Rows) error {
 	defer func() {
 		rows.Close()
@@ -20,6 +28,7 @@ func (this *DataTable) init(rows *sql.Rows) error {
 	if errMsg != nil {
 		return errMsg
 	}
+	this.columnNames = make(map[string]int)
 	for index, val := range tmpColumns {
 		this.columnNames[val] = index
 	}
@@ -27,17 +36,28 @@ func (this *DataTable) init(rows *sql.Rows) error {
 	// 读取行数据
 	this.rowData = make([]*DataRow, 0)
 	columnCount := len(this.columnNames)
-	for rows.Next() {
-		rowCells := make([]interface{}, columnCount)
-		rows.Scan(rowCells...)
 
-		this.rowData = append(this.rowData, newDataRow(this, rowCells))
+	args := make([]interface{}, columnCount)
+	for rows.Next() {
+		values := make([]interface{}, columnCount)
+		for i := 0; i < columnCount; i++ {
+			args[i] = &values[i]
+		}
+		rows.Scan(args...)
+
+		this.rowData = append(this.rowData, newDataRow(this, values))
 	}
 
 	return nil
 }
 
-func (this *DataTable) CellByIndex(rowIndex int, cellIndex int) (interface{}, error) {
+// 获取原始单元格值（一般为:string或[]byte）
+// rowIndex:行序号
+// cellIndex:单元格序号
+// 返回值:
+// interface{}:原始单元格值（一般为:string或[]byte）
+// error:获取错误信息
+func (this *DataTable) OriginCellValueByIndex(rowIndex int, cellIndex int) (interface{}, error) {
 	if len(this.rowData) <= rowIndex {
 		return nil, errors.New("row out of range")
 	}
@@ -47,23 +67,65 @@ func (this *DataTable) CellByIndex(rowIndex int, cellIndex int) (interface{}, er
 		return nil, errors.New("column out of range")
 	}
 
-	return rowItem.cells[cellIndex], nil
+	return rowItem.OriginCellValue(cellIndex)
 }
 
-func (this *DataTable) CellByCellName(rowIndex int, cellName string) (interface{}, error) {
+// 获取原始单元格值（一般为:string或[]byte）
+// rowIndex:行序号
+// cellIndex:单元格序号
+// 返回值:
+// interface{}:原始单元格值（一般为:string或[]byte）
+// error:获取错误信息
+func (this *DataTable) OriginCellValueByCellName(rowIndex int, cellName string) (interface{}, error) {
 	if len(this.rowData) <= rowIndex {
 		return nil, errors.New("row out of range")
 	}
 
-	cellIndex, isExist := this.columnNames[cellName]
-	if isExist == false {
-		return nil, errors.New("column no Exist")
+	rowItem := this.rowData[rowIndex]
+
+	return rowItem.OriginCellValueByName(cellName)
+}
+
+// 获取字符串的单元格值（有可能为nil）
+// rowIndex:行序号
+// cellIndex:单元格序号
+// 返回值:
+// interface{}:字符串的单元格值（有可能为nil）
+// error:获取错误信息
+func (this *DataTable) CellValueByIndex(rowIndex int, cellIndex int) (interface{}, error) {
+	if len(this.rowData) <= rowIndex {
+		return nil, errors.New("row out of range")
 	}
 
 	rowItem := this.rowData[rowIndex]
-	return rowItem.cells[cellIndex], nil
+	if len(rowItem.cells) <= cellIndex {
+		return nil, errors.New("column out of range")
+	}
+
+	return rowItem.CellValue(cellIndex)
 }
 
+// 获取字符串的单元格值（有可能为nil）
+// rowIndex:行序号
+// cellIndex:单元格序号
+// 返回值:
+// interface{}:字符串的单元格值（有可能为nil）
+// error:获取错误信息
+func (this *DataTable) CellValueByName(rowIndex int, cellName string) (interface{}, error) {
+	if len(this.rowData) <= rowIndex {
+		return nil, errors.New("row out of range")
+	}
+
+	rowItem := this.rowData[rowIndex]
+
+	return rowItem.CellValueByName(cellName)
+}
+
+// 获取行对象
+// rowIndex:行序号
+// 返回值:
+// *DataRow:行对象
+// error:错误信息
 func (this *DataTable) Row(rowIndex int) (*DataRow, error) {
 	if len(this.rowData) <= rowIndex {
 		return nil, errors.New("row out of range")
@@ -72,6 +134,10 @@ func (this *DataTable) Row(rowIndex int) (*DataRow, error) {
 	return this.rowData[rowIndex], nil
 }
 
+// 根据列名获取列序号
+// cellName:列名
+// 返回值:
+// int:列序号
 func (this *DataTable) cellIndex(cellName string) int {
 	cellIndex, isExist := this.columnNames[cellName]
 	if isExist == false {
@@ -81,12 +147,39 @@ func (this *DataTable) cellIndex(cellName string) int {
 	return cellIndex
 }
 
-func (this *DataTable) Len() int {
+// 获取所有列的名字
+// 返回值:
+// []string:列字段名集合
+func (this *DataTable) Columns() []string {
+	result := make([]string, len(this.columnNames))
+	for key, val := range this.columnNames {
+		result[val] = key
+	}
+
+	return result
+}
+
+// 获取列数量
+// 返回值:
+// int:列数量
+func (this *DataTable) ColumnCount() int {
+	return len(this.columnNames)
+}
+
+// 获取数据行数
+// 返回值:
+// int:行数
+func (this *DataTable) RowCount() int {
 	return len(this.rowData)
 }
 
+// 新建数据表对象
+// rows:数据行对象
+// 返回值：
+// *DataTable:数据表对象
+// error:错误信息
 func NewDataTable(rows *sql.Rows) (*DataTable, error) {
-	table := new(DataTable)
+	table := &DataTable{}
 	errMsg := table.init(rows)
 	if errMsg != nil {
 		return nil, errMsg
